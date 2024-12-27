1b0ffa51837463ef6e236f362e0c943233461998
J.Hale <1435319+J-HaleOf76@users.noreply.github.com>
2024-12-27 15:20:19 -0500

    Fixing file structure and removing utils/ folder

---
+++ lua/util/config.lua
@@ -0,0 +1,47 @@
+--[[ This is where I set the colorscheme ]]--
+--TODO: Make a wallust theme and save.
+--
+-- NeoPywal has several theme builtin and more that it can apply in the config.
+--
+--
+
+--Set the main theme here
+return {
+  -- colorscheme = "tokyodark",
+  colorscheme = "tokyo-dark-terminal",
+  -- colorscheme = "base16-tokyodark",
+  -- colorscheme = "tokyonight-night",
+   -- colorscheme = "base16-3024",
+  -- colorscheme = "tokyonight",
+  -- colorscheme = "rose-pine",
+  -- variant = "main",
+    -- colorscheme = "catppuccin",
+    -- variant = "mocha",
+  -- colorscheme = "kanagawa",
+  -- variant = "dragon",
+  -- TODO: SAVE SOME OF THESE PYWAL & WALLUST THEMES To be used with the rest of WALLUST OR PYWAL
+  -- Rose-Pine Kanagawa Tokyo-Dark-Terminal TokyoNight-Night
+  --
+  -- NeoPywal has other builtin themes
+  -- colorscheme = "neopywal",
+  -- variant = "dark",
+  --
+  variant = "dark",
+	transparent = false,
+  --[[TODO: Where do these other calls come from? ]]--
+  --
+	-- filemanager = "neo-tree", -- supported: mini.files, oil, neo-tree
+	-- keymapper = "which-key", -- supported: mini.clue, which-key
+	-- journal_dir = os.getenv("HOME") .. "/MEGASync/NOTES/dailies/",
+	notes = os.getenv("HOME") .. "/MEGASync/NOTES/dailies/",
+	-- startup = {
+	-- 	show_diagnostics = true,
+	-- 	show_git_blame = false,
+	-- },
+	-- integrations = {
+	-- 	notes = {
+	-- 		enabled = true,
+	-- 	},
+	-- },
+}
+
